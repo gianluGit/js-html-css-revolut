a072f4e89f788d232c0a979f0c3ec90d145ac75f
Gianluca <gianlucarecchia1993@gmail.com>
2020-07-07 17:03:32 +0200

    revolut- jumbotron e modifiche hamburger menu

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// dropdown versione lg
 $(document).ready(function() {
   var linkMenu = $('.navbar-main-center li > a');
   linkMenu.mouseenter(function() {
@@ -12,17 +13,20 @@ $(document).ready(function() {
   var menuIcon = $('#menu-icon');
   menuIcon.click(function() {
     $('.hamburger-menu').addClass('active');
+    $('.jumbo-left, .jumbo-right').addClass('none');
   });
 
   // evento click per chiudere l'hamburger menu
   var closeMenu = $('.hamburger-menu #close');
   closeMenu.click(function() {
     $('.hamburger-menu').removeClass('active');
+    $('.jumbo-left, .jumbo-right').removeClass('none');
   });
 
   // evento click sul dropdown-hamburger-menu
   var mainLinkH = $('.hamburger-menu .main-link');
   mainLinkH.click(function() {
+    $('.dropdown-hamburger').slideUp();
     $(this).next('.dropdown-hamburger').slideToggle();
   });
 });
